b0859a645514dd464289ce56474481aac201351b
Danyakotik228 <45770906+Danyakotik228@users.noreply.github.com>
2019-04-09 12:02:19 +0300

    Update myfunctest.h

--- tests/myfunctest.h
+++ tests/myfunctest.h
@@ -12,13 +12,26 @@ TEST(myfuncTest, num0) {
     ASSERT_EQ(myfunc(1, 1, 1).flag, 0);
 }
 
-TEST(myfuncTest, greather2) {
-    ASSERT_EQ(myfunc(10, 10, 10).flag, 0);
-    ASSERT_EQ(myfunc(10, 9, 8).flag, 0);
+TEST(myfuncTest, num1) {
+    ASSERT_EQ(myfunc(0, 5, 5).flag, 1);
+
 }
 
-TEST(myfuncTest, negative) {
+TEST(myfuncTest, num2) {
     ASSERT_EQ(myfunc(-1, -5, -3).flag, 2);
+}
+
+TEST(myfuncTest, num3) {
+    ASSERT_EQ(myfunc(0, 0, 0).flag, 3);
+}
+
+TEST(myfuncTest, num4) {
+    ASSERT_EQ(myfunc(1, -5, 6).x1, 2);
+    ASSERT_EQ(myfunc(1, -5, 6).x2, 3);
+}
+
+TEST(myfuncTest, num5) {
+    ASSERT_EQ(myfunc(2, 4, 2).flag, 1);
 }
 
 #endif // MYFUNCTEST_H
